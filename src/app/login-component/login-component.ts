f2f1f4f13fcdd83a56a1fb3cb57f8b8e054934b2
Filipe Pacheco <filipesmpacheco@gmail.com>
2024-04-23 20:00:57 -0300

    Login de usuario

--- src/app/login-component/login-component.ts
+++ src/app/login-component/login-component.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, inject, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import {
   IonHeader,
@@ -12,6 +14,8 @@ import {
   IonIcon,
   IonCheckbox,
   NavController,
+  IonInput,
+  AlertController,
 } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { chevronBackOutline, personOutline } from 'ionicons/icons';
@@ -33,13 +37,27 @@ import { chevronBackOutline, personOutline } from 'ionicons/icons';
     IonButton,
     IonIcon,
     IonCheckbox,
+    IonInput,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
 })
 export class LoginComponent implements OnInit {
   public folder!: string;
   private activatedRoute = inject(ActivatedRoute);
-  constructor(public router: Router) {
+  signInForm: FormGroup;
+  isAlertOpen = false;
+  
+  constructor(public router: Router, public formBuilder: FormBuilder, public alertController: AlertController, private http: HttpClient) {
     addIcons({ personOutline, chevronBackOutline });
+
+    this.signInForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$')]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      student: [false, Validators.requiredTrue],
+      teacher: [false, Validators.requiredTrue]
+    }, { validators: [this.checkCheckboxes] })
   }
 
   ngOnInit() {
@@ -48,5 +66,80 @@ export class LoginComponent implements OnInit {
 
   goToSignUp() {
     this.router.navigateByUrl('cadastro');
+  }
+
+  private checkCheckboxes(group: FormGroup) {
+    let checkbox1 = group.get('student')?.value;
+    let checkbox2 = group.get('teacher')?.value;
+    return checkbox1 !== checkbox2 ? null : { notExclusive: true };
+  }
+
+  async showAlert(title: string, message: string) {
+    const alert = await this.alertController.create({
+      header: title,
+      message: message,
+      buttons: [
+        {
+          text: 'OK',
+          handler: () => {
+            this.isAlertOpen = false;
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+    this.isAlertOpen = true;
+  }
+
+
+  onCheckboxChange(checkbox: string) {
+    if(checkbox === 'student') {
+      this.signInForm.get('teacher')?.setValue(false);
+      this.signInForm.get('teacher')?.clearValidators();
+      this.signInForm.get('teacher')?.updateValueAndValidity();
+    } else {
+      this.signInForm.get('student')?.setValue(false);
+      this.signInForm.get('student')?.clearValidators();
+      this.signInForm.get('student')?.updateValueAndValidity();
+    }
+  }
+
+  signIn() {
+    const payload = {
+      email: this.signInForm.value.email,
+      password: this.signInForm.value.password
+    };
+
+    if(this.signInForm.value.student) {
+      this.http.post('http://localhost:3000/alunos/login', payload).subscribe({
+        next: (response) => {
+          const message = (response as any).message;
+          const title = 'Sucesso!';
+          this.showAlert(title, message);
+        },
+        error: (error) => {
+          const message = 'Tente novamente';
+          const title = error.error.error;
+          this.showAlert(title, message);
+        }
+      });
+    } else {
+      this.http.post('http://localhost:3000/tutores/login', payload).subscribe({
+        next: (response) => {
+          const message = (response as any).message;
+          const title = 'Sucesso!';
+          this.showAlert(title, message);
+        },
+        error: (error) => {
+          console.log(error)
+          const message = 'Tente novamente';
+          const title = error.error.error;
+          this.showAlert(title, message);
+        }
+      });
+    }
+
+    this.isAlertOpen = true;
   }
 }
